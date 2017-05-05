6230f057da8ef5cb17e662c4ad14519a217c3660
kllilizxc <541796726@qq.com>
2017-05-05 11:29:24 +0800

    little

--- HairSimTraining/HairRaw.cpp
+++ HairSimTraining/HairRaw.cpp
@@ -31,27 +31,27 @@ void HairRaw::build(std::string filename)
 	std::ifstream is(filename, std::ifstream::binary);
 
 	int totalParticleSize;
-	is.read((char *)&totalParticleSize, sizeof(int));
+	is.read(reinterpret_cast<char *>(&totalParticleSize), sizeof(int));
 
 	frames.push_back(new HairFrame());
 	restState = frames.at(0);
-	for (int i = 0; i < totalParticleSize; i++)
+	for (auto i = 0; i < totalParticleSize; i++)
 	{
 		vec position(3);
-		is.read((char *)&position.at(0), sizeof(double));
-		is.read((char *)&position.at(1), sizeof(double));
-		is.read((char *)&position.at(2), sizeof(double));
+		is.read(reinterpret_cast<char *>(&position.at(0)), sizeof(double));
+		is.read(reinterpret_cast<char *>(&position.at(1)), sizeof(double));
+		is.read(reinterpret_cast<char *>(&position.at(2)), sizeof(double));
 		restState->getParticles().at(i).setPosition(position);
 	}
 
 	int totalStrandSize;
-	is.read((char *)&totalStrandSize, sizeof(int));
+	is.read(reinterpret_cast<char *>(&totalStrandSize), sizeof(int));
 
 	int index = 0;
-	for (int i = 0; i < totalStrandSize; i++)
+	for (auto i = 0; i < totalStrandSize; i++)
 	{
 		int particleSize;
-		is.read((char *)&particleSize, sizeof(int));
+		is.read(reinterpret_cast<char *>(&particleSize), sizeof(int));
 
 		strands.at(i).setBegin(index);
 		strands.at(i).setEnd(index + particleSize);
